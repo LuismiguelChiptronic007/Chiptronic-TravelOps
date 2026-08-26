e21ddb2c33b866b78b50147700c3fb6fab9c7eeb
Luis Miguel <luismiguel.oliveira@chiptronic.com.br>
2026-08-26 10:16:48 -0300

    Atualização

--- Frontend/js/mapa-operacional.js
+++ Frontend/js/mapa-operacional.js
@@ -519,24 +519,18 @@ async function loadEstado() {
 }
 
 function initMap() {
+  const tileOptions = {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    maxZoom: 19,
+  };
   const tileDark = L.tileLayer(
-    "https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png",
-    {
-      attribution:
-        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/attributions">CARTO</a>',
-      subdomains: "abcd",
-      maxZoom: 19,
-    },
+    "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+    tileOptions,
   );
-
   const tileLight = L.tileLayer(
-    "https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png",
-    {
-      attribution:
-        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/attributions">CARTO</a>',
-      subdomains: "abcd",
-      maxZoom: 19,
-    },
+    "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+    tileOptions,
   );
 
   const prefersDark =
